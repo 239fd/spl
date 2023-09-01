bb7ba3158b8dabf500f7d00965bf9d46f0b243c0
239fd <126984519+239fd@users.noreply.github.com>
2023-09-01 16:47:29 +0300

    Update main.py

--- firsttask/first/main.py
+++ firsttask/first/main.py
@@ -1,16 +1,13 @@
 years = ['Обезьяна', 'Петух', 'Собака', 'Свинья', 'Мышь', 'Бык', 'Тигр', 'Кролик', 'Дракон','Змея', 'Лошадь', 'Овца']
 
 while True:
-    year = str(input("Введите год: " ))
-    if(year.isalpha()):
-        print("Mistake")
+    year = input("Введите год: " )
+    if(year.isalpha() or len(year) == 0 or year < "0"):
+        print("Ошибка")
         break
     elif(year == "0"):
         print("Конец")
         break
-    elif(year < "0"):
-        print("Mistake")
-        break2
     else:
         newyear = int(year)
         for i in range(0, 12):
