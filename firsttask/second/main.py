a0ba56ddb68ab2a596e5ba7c1c1576c412ea25d1
239fd <126984519+239fd@users.noreply.github.com>
2023-09-02 20:07:00 +0300

    Update main.py

--- firsttask/second/main.py
+++ firsttask/second/main.py
@@ -1,13 +1,18 @@
-while True:
-    str = input("Введите строку: ")
-    if(str.isdigit() or len(str) == 0):
-        print("Ошибка")
-        break
-    else:
-        newstr = str.split(" ")
-        count = 0;
-        for i in range(len(newstr)):
-            if(len(newstr[i]) % 2 != 0):
-                count += 1
-    print(*[f'{x} - {str.count(x)}' for x in set(str) if x.isalpha()], sep='\n')
+string = input("Введите строку: ")
+list = string.split()
+count = 0;
+def is_text(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+if(is_text(''.join(list)) or len(string) == 0 or not ''.join(list).isalpha()):
+    print("Ошибка")
+else:
+    newstr = string.split(" ")
+    for i in range(len(newstr)):
+        if(len(newstr[i]) % 2 != 0):
+            count += 1
+    print(*[f'{x} - {string.count(x)}' for x in set(string) if x.isalpha()], sep='\n')
     print(f'Количесвто нечетных слов: {count}')
