e681782f5e5ff22cf8f6a324943acae4f35532bd
239fd <126984519+239fd@users.noreply.github.com>
2023-09-02 19:21:29 +0300

    Update2 main.py

--- firsttask/first/main.py
+++ firsttask/first/main.py
@@ -1,15 +1,18 @@
 years = ['Обезьяна', 'Петух', 'Собака', 'Свинья', 'Мышь', 'Бык', 'Тигр', 'Кролик', 'Дракон','Змея', 'Лошадь', 'Овца']
+def is_int(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
 
-while True:
-    year = input("Введите год: " )
-    if(year.isalpha() or len(year) == 0 or year < "0"):
-        print("Ошибка")
-        break
-    elif(year == "0"):
-        print("Конец")
-        break
-    else:
-        newyear = int(year)
-        for i in range(0, 12):
-            if newyear % 12 == i:
-                print(years[i])
+year = input("Введите год: " )
+if(not is_int(year) or year < "0"):
+    print("Ошибка")
+elif(year == "0"):
+    print("Конец")
+else:
+    newyear = int(year)
+    for i in range(0, 12):
+        if newyear % 12 == i:
+            print(years[i])
