312b580dee0b5299355e88d29df8932eea2bc6e3
239fd <126984519+239fd@users.noreply.github.com>
2023-09-02 20:10:49 +0300

    Update main.py

--- firsttask/third/main.py
+++ firsttask/third/main.py
@@ -1,15 +1,18 @@
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
 s = input('Введите список: ')
 list = s.split()
 counter = 0
-if(len(s) == 0 or s.isalpha()):
+if(len(s) == 0 or s.isalpha() or not is_number(''.join(list))):
     print("Ошибка")
-elif('1'.join(list).isdigit()):
+else:
     for i in range(len(list)):
         for j in range(i + 1, len(list)):
             if list[i] == list[j]:
                 counter += 1
     print(counter)
-else:
-    for i in range(len(list)):
-        if (list[i].isalpha()):
-            print("Ошибка")
