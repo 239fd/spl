fc9a0c9eb31ea46f41dac5ee0ad024c7ea3d4253
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2023-11-16 23:50:07 +0300

    Update main.py

--- fifthtask/first/first/main.py
+++ fifthtask/first/first/main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import numpy as np
+
+a = 0.3
+b = -21.17
+
+def ctg(x):
+    return 1 / np.tan(x)
+
+
+y = ctg(np.pi/4 - 1)**4 + (a + 1.5)**(1/3) + (a-b)**8 - b/(np.arcsin(np.abs(a)**2))
+
+print(f"Ответ равен {y}")
